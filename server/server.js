8e65096857b5975f0bce549caee3f87dcee6646a
Phat <dvphat92@gmail.com>
2018-10-02 17:07:36 +0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,9 +14,25 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user was connected'); // eslint-disable-line
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', message => {
         console.log('createMessage', message); // eslint-disable-line
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
